e933b9ddbdd9fff7247f7937306c445ade98957b
Schmidt Nico (XC-AS/ERC32) <nico.schmidt@de.bosch.com>
2024-04-19 16:19:27 +0200

    add comment to build script

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ conan export libc/conanfile.py
 
 conan graph info --requires=libc/0.1 --lockfile-out=output.lock --format=json > graph.json
 conan graph build-order --requires=libc/0.1 --lockfile=output.lock --order-by=configuration --format=json --build=missing > build_order.json
+
+# This line is taken from the build order output json
 conan install --require=libb/0.1 --build=libb/0.1 --lockfile-overrides="{'zlib/1.2.12': ['zlib/1.2.13']}" --lockfile=output.lock
 
